a4bd79409edc89424aaf142cfb98e62ea195ca98
Mathilde Hétru <mathilde.hetru@gmail.com>
2026-02-09 14:16:01 +0100

    feat: nuxt-content

---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p>Weena et la légende de Noor</p>
+    <HeroItem />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/content', 'nuxt-studio'],
+  content: {},
+  studio: {
+    repository: {
+      provider: 'github',
+      owner: 'mathhetru',
+      repo: 'weeki',
+      branch: 'feat/nuxt-content',
+    },
+  },
 })
