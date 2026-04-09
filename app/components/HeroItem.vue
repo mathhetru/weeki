99e4506823f79965c4d5af6c4ba5770c4bd6a488
Mathilde Hétru <mathilde.hetru@gmail.com>
2026-04-09 18:44:04 +0200

    feat: wip responsive

--- app/components/HeroItem.vue
+++ app/components/HeroItem.vue
@@ -4,7 +4,7 @@
     class="mx-auto w-full sm:w-[70%] lg:w-[75%] max-w-7xl flex flex-col lg:flex-row gap-4 backdrop-blur-xs rounded-4xl bg-white/20 p-10 shadow-lg items-center"
   >
     <div
-      class="w-full sm:w-[80%] lg:w-[30%] flex flex-col gap-4 text-center justify-center lg:mx-12 py-6 lg:py-0 lg:h-150"
+      class="w-full sm:w-[80%] lg:w-[30%] flex flex-col gap-4 text-center justify-center lg:mx-12 py-6 lg:py-0"
     >
       <h1 class="text-3xl lg:text-4xl font-semibold font-heading golden">
         {{ data?.hero?.heading }}
@@ -12,7 +12,7 @@
       <p>{{ data?.hero?.description }}</p>
     </div>
     <div
-      class="w-full md:w-[65%] lg:w-[70%] grid grid-rows-2 grid-cols-3 gap-3 lg:gap-6 h-80 sm:h-100 lg:h-150"
+      class="w-full sm:w-[90%] lg:w-[70%] grid grid-rows-2 grid-cols-3 gap-3 lg:gap-6 h-80 sm:h-100 lg:h-150"
     >
       <NuxtLink
         v-for="(feature, index) in data.features"
@@ -33,7 +33,9 @@
         <div
           class="absolute inset-x-0 top-0 h-16 lg:h-30 bg-linear-to-b from-black/70 to-transparent z-10"
         />
-        <h3 class="absolute z-20 text-white p-4 lg:p-6 text-xs lg:text-lg drop-shadow-lg">
+        <h3
+          class="absolute z-20 text-white px-2 py-3 lg:px-6 lg:py-6 text-xs lg:text-lg drop-shadow-lg"
+        >
           {{ feature?.title }}
         </h3>
       </NuxtLink>
@@ -51,7 +53,7 @@
   const { data } = await useAsyncData('home-hero', () => queryCollection('pages').first())
 
   const getGridClass = (index: number) => {
-    const baseClass = 'relative rounded-3xl overflow-hidden'
+    const baseClass = 'relative rounded-lg lg:rounded-2xl overflow-hidden'
     const gridClasses: Record<number, string> = {
       0: 'row-span-1 col-start-1 row-start-1 sm:mt-24 mt-12',
       1: 'row-span-3 col-start-2 row-start-1',
